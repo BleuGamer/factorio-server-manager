fa404125d1a24c980b833906538b015304633095
Lance Wallis <bleudeveloper@gmail.com>
2019-12-08 12:04:19 -0600

    More discord testing, implimentation based on factocord.

---
+++ src/discord_helpers.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/hpcloud/tail"
+)
+
+/*
+ * Under the use of the MIT license, these implementations and modifications
+ * are made from works originating from Allen Lydiard:
+ *
+ * https://github.com/FactoKit/FactoCord
+ */
+
+// Pipe chat to discord.
+func Chat(s *discordgo.Session, m *discordgo.MessageCreate) {
+	for {
+		t, err := tail.TailFile(config.FactorioLog, tail.Config{Follow: true})
+		if err != nil {
+			log.Println("Could not tail factorio.log", err)
+		}
+		for line := range t.Lines {
+			if strings.Contains(line.Text, "[CHAT]") || strings.Contains(line.Text, "[JOIN]") || strings.Contains(line.Text, "[LEAVE]") {
+				if !strings.Contains(line.Text, "<server>") {
+
+					if strings.Contains(line.Text, "[JOIN]") ||
+						strings.Contains(line.Text, "[LEAVE]") {
+						TmpList := strings.Split(line.Text, " ")
+						// Don't hard code the channelID! }:<
+						s.ChannelMessageSend(config.DiscordChannelId, fmt.Sprintf("%s", strings.Join(TmpList[3:], " ")))
+					} else {
+
+						TmpList := strings.Split(line.Text, " ")
+						TmpList[3] = strings.Replace(TmpList[3], ":", "", -1)
+						if strings.Contains(strings.Join(TmpList, " "), "@") {
+							index := LocateMentionPosition(TmpList)
+
+							for _, position := range index {
+								User := SearchForUser(TmpList[position])
+
+								if User == nil {
+									continue
+								}
+								TmpList[position] = User.Mention()
+							}
+
+						}
+
+						s.ChannelMessageSend(config.DiscordChannelId, fmt.Sprintf("<%s>: %s", TmpList[3], strings.Join(TmpList[4:], " ")))
+					}
+				}
+			}
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+// SearchForUser searches for the user to be mentioned.
+func SearchForUser(name string) *discordgo.User {
+	name = strings.Replace(name, "@", "", -1)
+	for _, user := range Users {
+		if strings.ToLower(user.Nick) == strings.ToLower(name) ||
+			strings.ToLower(user.User.Username) == strings.ToLower(name) {
+			return user.User
+		}
+	}
+	return nil
+}
+
+// LocateMentionPosition locates the position in a string list for the discord mention.
+func LocateMentionPosition(List []string) []int {
+	positionlist := []int{}
+	for i, String := range List {
+		if strings.Contains(String, "@") {
+			positionlist = append(positionlist, i)
+		}
+	}
+	return positionlist
+}
+
+// UserList is a struct for member info.
+type UserList struct {
+	UserID string
+	Nick   string
+	User   *discordgo.User
+}
+
+// Users is a slice of UserList.
+var Users []UserList
+
+// CacheDiscordMembers caches the users list to be searched.
+func CacheDiscordMembers(s *discordgo.Session) {
+	// Clear the users list
+	Users = nil
+
+	GuildChannel, err := s.Channel(config.DiscordChannelId)
+	if err != nil {
+		log.Println("An error occurred when attempting to read the Discord Guild Details: ", err)
+	}
+	GuildID := GuildChannel.GuildID
+	members, err := s.State.Guild(GuildID)
+	if err != nil {
+		log.Println("An error occurred when attempting to read the Discord Guild Members Details: ", err)
+	}
+	for _, member := range members.Members {
+		Users = append(Users, UserList{UserID: member.User.ID, Nick: member.Nick,
+			User: member.User})
+	}
+}
--- src/discord_init.go
+++ src/discord_init.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"strings"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
@@ -44,8 +45,58 @@ func initDiscord(token string, channel string) {
 		log.Println("error opening connection,", err)
 		return
 	}
+	bot.AddHandlerOnce(Chat)
 	time.Sleep(3 * time.Second)
 	log.Println("Discord launched successfully!")
-
 	bot.UpdateStatus(0, "Factorio")
+
+	/*
+		embedme := &discordgo.MessageEmbed{
+			Author:      &discordgo.MessageEmbedAuthor{},
+			Color:       0xb87333, // Copper
+			Description: "Factorio Message Test Embed",
+			Fields: []*discordgo.MessageEmbedField{
+				&discordgo.MessageEmbedField{
+					Name:   "Field",
+					Value:  "Value",
+					Inline: false,
+				},
+			},
+			Image: &discordgo.MessageEmbedImage{
+				URL: "https://wiki.factorio.com/images/Rocket_silo_entity.png",
+			},
+			Thumbnail: &discordgo.MessageEmbedThumbnail{
+				URL: "https://mods-data.factorio.com/assets/832490ba5d54b75061d9f5c959c75dfa883613ab.thumb.png",
+			},
+			Timestamp: time.Now().Format(time.RFC3339), // Discord wants ISO8601; RFC3339 is an extension of ISO8601 and should be completely compatible.
+			Title:     "Factorio Announcement: ",
+		}
+
+		Session.ChannelMessageSendEmbed(config.DiscordChannelId, embedme)
+	*/
+}
+
+// Send discord messages to game.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	log.Print("[" + m.Author.Username + "] " + m.Content)
+
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.ChannelID == config.DiscordChannelId {
+		if strings.HasPrefix(m.Content, config.DiscordPrefix) {
+			//command := strings.Split(m.Content[1:len(m.Content)], " ")
+			//name := strings.ToLower(command[0])
+			// TODO: commands
+			// commands.RunCommand(name, s, m)
+			return
+		}
+		// Pipes normal chat allowing it to be seen ingame
+		//_, err := io.WriteString(Pipe, fmt.Sprintf("[Discord] <%s>: %s\r\n", m.Author.Username, m.ContentWithMentionsReplaced()))
+		//if err != nil {
+		//	log.Println("An error occurred when attempting to pass Discord chat to in-game Details: ", err)
+		//}
+		return
+	}
 }
